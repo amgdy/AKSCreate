c7dbb80f683161cd76308628306e155ce16cc035
Mohammed Omar <MomarSE@hotmail.com>
2021-03-22 20:19:26 +0300

    Create AKSInit.sh

---
+++ AKSInit.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/mohaom/AKSCreate/main/AKSCreate.sh
+wget https://raw.githubusercontent.com/mohaom/AKSCreate/main/AKSRoleAssign.sh
+wget https://raw.githubusercontent.com/mohaom/AKSCreate/main/RoleBindingNameSpace.yaml
+wget https://raw.githubusercontent.com/mohaom/AKSCreate/main/RoleFullAccess.yaml
+wget https://raw.githubusercontent.com/mohaom/AKSCreate/main/aad-user-cluster-admin-binding.yaml
+
+chmod +x ./AKSCreate.sh
+chmod +x ./AKSRoleAssign.sh
