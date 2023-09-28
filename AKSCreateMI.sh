1327e65478d584a4c18af0cc8d4c2b76be6e92b7
Ahmed Magdy <amagdy@microsoft.com>
2023-09-28 19:34:19 +0300

    Added userpool to the script

--- AKSCreateMI.sh
+++ AKSCreateMI.sh
@@ -1,23 +1,23 @@
 GREEN="\033[0;32m"
 echo -e "$GREEN Hi!, This script will help you create Azure Kubernetes Services"
-echo -e "$GREEN These are the prerequisistes needed:"
+echo -e "$GREEN These are the prerequisistes needed: "
 echo -e "$GREEN - Global Admin Permission on ADD"
 echo -e "$GREEN - Subscription Owner"
 echo -e "$GREEN Subnet created to Land AKS"
 echo -e "$GREEN -----------------------------------"
-echo -e "$GREEN Please enter the name of the cluster:"
+echo -e "$GREEN Please enter the name of the cluster: "
 read clusterName
 echo export clusterName=$clusterName >>./var.txt
-echo -e "$GREEN Please enter the location by example 'westeurope/uaenorth':"
+echo -e "$GREEN Please enter the cluster location: (Example: westeurope or uaenorth)"
 read location
 echo export location=$location >>./var.txt
-echo -e "$GREEN Please enter the Resource-Group for the VNET:"
+echo -e "$GREEN Please enter the Resource-Group for the VNET: "
 read vnetrg
 echo export vnetrg=$vnetrg >>./var.txt
-echo -e "$GREEN Please enter the vnet name:"
+echo -e "$GREEN Please enter the vnet name: "
 read vnetname
 echo export vnetname=$vnetname >>./var.txt
-echo -e "$GREEN Please enter the Subnet name:"
+echo -e "$GREEN Please enter the Subnet name: "
 read subname
 echo export subname=$subname >>./var.txt
 
@@ -31,7 +31,7 @@ if [ $subnetcreated == 'y' ]; then
   echo export SUBNET_ID=$SUBNET_ID >>./var.txt
 else
 
-  echo -e "$GREEN Ok! What is the address-prefix for the subnet ? by example '10.179.128.0/21'"
+  echo -e "$GREEN Ok! What is the address-prefix for the subnet? (Example: 10.179.128.0/21)"
   read addressPrefix
   echo -e "$GREEN Creating subnet for AKS cluster..."
   VNET_ID=$(az network vnet show --resource-group ${vnetrg} --name $vnetname --query id -o tsv)
@@ -44,7 +44,7 @@ fi
 echo -e "$GREEN Geting tenant ID..."
 tenantId=$(az account show --query tenantId -o tsv)
 
-echo -e "$GREEN do you have a predefined Admin Group ? y/n"
+echo -e "$GREEN do you have a predefined Admin Group ? [y/n]"
 read createADGroup
 if [ $createADGroup == 'y' ]; then
   echo -e "$GREEN What is the name of the predefined AD Group?"
@@ -65,20 +65,35 @@ else
   echo export GROUP_ID=$GROUP_ID >>./var.txt
 fi
 
-echo -e "$GREEN Creating Cluster..."
-echo -e "$GREEN How many nodes you require for the cluster ?"
+echo -e "$GREEN Now let's configure System and User node pools for the cluster: "
+echo -e "$GREEN System node pools serve the primary purpose of hosting critical *system pods* such as CoreDNS, konnectivity, metrics-server..."
+echo -e "$GREEN System node pools count: (2 or higher is recommended)"
 read nodecount
 echo export nodecount=$nodecount >>./var.txt
-echo -e "$GREEN Node Count will be $nodecount.."
+echo -e "$GREEN System node pools count will be $nodecount.."
 
-echo -e "$GREEN what is node vm size ? example Standard_D8s_v3"
+echo -e "$GREEN System node pools VM Size: (example: Standard_DS4_v2)"
 read nodeSize
 echo export nodeSize=$nodeSize >>./var.txt
-echo -e "$GREEN Specify AKS version or 1.27.3"
+echo -e "$GREEN System node pools VM Size will be $nodeSize"
 
+echo -e "$GREEN User node pools serve the primary purpose of hosting your application pods."
+
+echo -e "$GREEN User node pools count: (3 or higher odd number is recommended)"
+read userNodepoolCount
+echo export userNodepoolCount=$userNodepoolCount >>./var.txt
+echo -e "$GREEN User node pools count will be $userNodepoolCount."
+
+echo -e "$GREEN User node pools VM Size: (example: Standard_D8s_v3)"
+read userNodepoolSize
+echo export userNodepoolSize=$userNodepoolSize >>./var.txt
+echo -e "$GREEN User node pools VM Size will be $userNodepoolSize"
+
+echo -e "$GREEN Specify AKS version: (Example: 1.27.3)"
 read AKSVersion
 echo export AKSVersion=$AKSVersion >>./var.txt
-echo -e "$GREEN What is the Resource Group for the Cluster ?"
+
+echo -e "$GREEN What is the Resource Group for the Cluster?"
 read ResourceGroup
 echo export ResourceGroup=$ResourceGroup >>./var.txt
 # Assign subnet contributor permissions
@@ -89,21 +104,21 @@ read UManagedIdentity
 echo export UManagedIdentity=$UManagedIdentity >>./var.txt
 ManagedIdentityId=$(az identity create --name $UManagedIdentity --resource-group $ResourceGroup --query "id" | tr -d '"')
 
-echo -e "$GREEN Would this cluster host Windows Nodes ? y/n"
+echo -e "$GREEN Would this cluster host Windows Nodes ? [y/n]"
 read WindowsNode
 echo export WindowsNode=$WindowsNode >>./var.txt
 
 if [ $WindowsNode == 'y' ]; then
 
-  echo -e "$GREEN Please provide username for Windows Nodes ?"
+  echo -e "$GREEN Please provide username for Windows Nodes?"
   read WindowsNodeUsername
 
-  echo -e "$GREEN Please provide Password for Windows Nodes ?"
+  echo -e "$GREEN Please provide Password for Windows Nodes?"
   read WindowsNodePassword
 
   echo export WindowsNodeUsername=$WindowsNodeUsername >>./var.txt
   echo export WindowsNodePassword=$WindowsNodePassword >>./var.txt
-
+  echo -e "$GREEN Creating Windows-based Cluster..."
   az aks create \
     --resource-group $ResourceGroup \
     --name $clusterName \
@@ -116,7 +131,6 @@ if [ $WindowsNode == 'y' ]; then
     --windows-admin-password $WindowsNodePassword \
     --network-plugin azure \
     --vnet-subnet-id $SUBNET_ID \
-    --docker-bridge-address 172.170.0.1/16 \
     --service-cidr 172.171.0.0/16 \
     --dns-service-ip 172.171.0.10 \
     --enable-aad \
@@ -124,11 +138,13 @@ if [ $WindowsNode == 'y' ]; then
     --aad-tenant-id $tenantId \
     --enable-managed-identity \
     --assign-identity $ManagedIdentityId \
-    --kubernetes-version $AKSVersion
+    --kubernetes-version $AKSVersion \
+    --nodepool-name systempool01
 
 else
+  echo -e "$GREEN Creating Linux-based Cluster..."
   az aks create \
-  --resource-group $ResourceGroup \
+    --resource-group $ResourceGroup \
     --name $clusterName \
     --location $location \
     --generate-ssh-keys \
@@ -137,7 +153,6 @@ else
     --vm-set-type VirtualMachineScaleSets \
     --network-plugin kubenet \
     --vnet-subnet-id $SUBNET_ID \
-    --docker-bridge-address 172.170.0.1/16 \
     --service-cidr 172.171.0.0/16 \
     --dns-service-ip 172.171.0.10 \
     --enable-aad \
@@ -145,16 +160,28 @@ else
     --aad-tenant-id $tenantId \
     --enable-managed-identity \
     --assign-identity $ManagedIdentityId \
-    --kubernetes-version $AKSVersion
+    --kubernetes-version $AKSVersion \
+    --nodepool-name systempool01
 
 fi
+
+echo -e "$GREEN Adding User Node Pools to the cluster..."
+
+az aks nodepool add \
+  --cluster-name $clusterName \
+  --name $userNodepoolCount \
+  --resource-group $ResourceGroup \
+  --mode User \
+  --node-count $userNodepoolCount \
+  --node-vm-size $userNodepoolSize \
+  --kubernetes-version $AKSVersion
 
 echo -e "$GREEN Congratulation AKS Cluster $clusterName has been created!"
 echo -e "$GREEN Logging into Cluster Now..."
 
 az aks get-credentials --name $clusterName --resource-group $ResourceGroup --overwrite-existing --admin
 
-echo -e "$GREEN Do you want to Attach Azure Container Registry to the cluster ? y/n"
+echo -e "$GREEN Do you want to Attach Azure Container Registry to the cluster ? [y/n]"
 read attachACR
 if [ $attachACR == 'y' ]; then
   echo -e "$GREEN What is the Container Register Name ?"
